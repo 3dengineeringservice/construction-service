7428f0ea2f19ef3f236d95fa9f027046d9d96267
raj <rraju12@gmail.com>
2021-01-31 22:13:13 +0000

    Fix build failed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div class="lds-circle"><div></div></div>
+    <h1>Loading...</h1>
       </header>
     </div>
   );
