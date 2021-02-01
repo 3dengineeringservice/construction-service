1906e8abe58d1d541bb3f19e36b41cb5f42efa39
raj <rraju12@gmail.com>
2021-02-01 00:08:58 +0000

    Add loading spoi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Loading...</h1>
+      <div class="lds-dual-ring"></div>
       </header>
     </div>
   );
